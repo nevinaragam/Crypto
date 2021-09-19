129916d01e2e86ed4fb1693ef4cc4cebe1902564
Not Set <Not Set>
2021-09-18 23:46:51 -0600

    Change Binance counts from USDT (Tether) to USD to match other exchanges.

--- bin_counts.py
+++ bin_counts.py
@@ -12,7 +12,7 @@ logging.basicConfig(format=log_format, level=logging.INFO, datefmt="%H:%M:%S")
 EXCHANGE = "Binance"
 
 # Set websocket
-SOCKET = "wss://stream.binance.com:9443/ws/btcusdt@bookTicker"
+SOCKET = "wss://stream.binance.com:9443/ws/btcusd@bookTicker"
 
 # Set seconds to run test
 RUNTIME = 15
